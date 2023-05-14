8af44d8b2e81fc07e65ab73c37411442cd72a07d
mina <baekmina2000@gmail.com>
2023-05-14 20:51:05 +0900

    Add test directory and index.html files

---
+++ javascript/javascript_variables/varaibles.js
@@ -0,0 +1,26 @@
+/*변수: 데이터를 담아 놓기 위해 이름표를 붙여놓은 공간*/
+
+/*변수 선언*/
+//var num;
+//변수 초기화
+//num=10;
+
+//변수 선언+초기화
+var num=10;
+num=20;
+console.log(num);
+
+//사용할 수 없는 변수명
+//숫자로 시작하는 경우 var 4 you=
+//특수문자
+
+//예약어: 프로그래밍 언어 자체적으로 사용할 단어 또는 키워드=>변수 선언 불가
+//변수명(식별자)
+//1.camel case-> ex)var sendEmailDate
+//2.snake case-> var send_email_date
+
+//주석(comment)
+//1.singleline//
+/*
+2.multiline
+    */
