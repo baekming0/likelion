a07f4984e8bbaa03403dfcebc11a40c6da008019
mina <baekmina2000@gmail.com>
2023-05-23 20:44:00 +0900

    10강

--- models/user.js
+++ models/user.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
-    name:{
+    name: {
         type: String,
         maxlength: 50
     },
     email: {
         type: String,
         trim: true,
-        unique:1
+        unique: 1
     },
     password: {
         type: String,
@@ -31,6 +34,23 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User',userSchema)
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
 
-module.exports = {User}
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+
+    }
+
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
