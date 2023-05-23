a90bdf932c1a0858e744dd080ebf50505f66d009
mina <baekmina2000@gmail.com>
2023-05-23 20:26:07 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/user");
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://boilerplate:abcd1234@boilerplate.dpqwzyw.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=> console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res) => {
+  const user = new User(req.body)
+  user.save()
+  .then(userInfo => {
+    return res.status(200).json({
+      success: true
+    });
+  })
+  .catch(err => {
+    return res.json({success: false, err});
+  });
+
 })
 
 app.listen(port, () => {
